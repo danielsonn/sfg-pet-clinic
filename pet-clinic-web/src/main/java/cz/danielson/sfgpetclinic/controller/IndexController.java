54fde45eb03c0f35efc48ab9ced761851b6f974b
dan <daniel.sacha@post.cz>
2019-12-08 07:17:31 +0100

    Add Pet Clinic Index Page and Controller close #8

--- pet-clinic-web/src/main/java/cz/danielson/sfgpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/cz/danielson/sfgpetclinic/controller/IndexController.java
@@ -2,12 +2,12 @@ package cz.danielson.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/", "", "index", "index.html"})
+    @GetMapping({"/", "", "index", "index.html"})
     public String index(Model model) {
         return "index";
     }
