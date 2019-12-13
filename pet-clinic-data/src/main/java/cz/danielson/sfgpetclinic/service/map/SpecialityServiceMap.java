75f0251181a162821573da47be5465ffa34ec794
dan <daniel.sacha@post.cz>
2019-12-13 07:32:23 +0100

    Create Speciality Map Service Impl close #32

--- pet-clinic-data/src/main/java/cz/danielson/sfgpetclinic/service/map/SpecialityServiceMap.java
+++ pet-clinic-data/src/main/java/cz/danielson/sfgpetclinic/service/map/SpecialityServiceMap.java
@@ -2,6 +2,8 @@ package cz.danielson.sfgpetclinic.service.map;
 
 import cz.danielson.sfgpetclinic.model.Speciality;
 import cz.danielson.sfgpetclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
 }
