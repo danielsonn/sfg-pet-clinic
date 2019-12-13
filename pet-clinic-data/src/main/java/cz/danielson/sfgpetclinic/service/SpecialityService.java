194af32a10d8f9b73922ef0b31dad3d4c8224a84
dan <daniel.sacha@post.cz>
2019-12-13 07:24:32 +0100

    Create Speciality Map Service Impl close #32

---
+++ pet-clinic-data/src/main/java/cz/danielson/sfgpetclinic/service/SpecialityService.java
@@ -0,0 +1,7 @@
+package cz.danielson.sfgpetclinic.service;
+
+import cz.danielson.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/cz/danielson/sfgpetclinic/service/map/SpecialityServiceMap.java
@@ -0,0 +1,7 @@
+package cz.danielson.sfgpetclinic.service.map;
+
+import cz.danielson.sfgpetclinic.model.Speciality;
+import cz.danielson.sfgpetclinic.service.SpecialityService;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+}
